138eabd52f123704a1c4e25a64e90c1f2b94adfb
Xin Feng <xinfeng@Xins-MacBook-Pro.local>
2018-10-21 12:07:56 +0800

    initial git repository setup

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ActivityIndicator, ListView, SectionList, StyleSheet, Text, View } from 'react-native';
+import HelloWorld from './components/HelloWorld';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <HelloWorld></HelloWorld>
+    );
+  }
+}
